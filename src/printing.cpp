0b0269c51c2b0b24ce4b23cbbb3287bfde3ad31b
fanialfi <fanialfirdaus@gmail.com>
2024-11-23 10:28:42 +0700

    printing data ke console

---
+++ src/printing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hallo kawan-kawan" << std::endl;
+  std::cout << "kali ini saya sedang belajar ";
+  std::cout << "dan tentu saja belajar C++\n";
+  std::cout << "saya harap ini mudah" << std::endl;
+  std::cin.get();  // cin.get() adalah console input, artinya akan mengambil input dari console, dan akan direturn jika sudah menekan enter 
+  // return value dari cin.get() adalah character
+  return 1;
+}
